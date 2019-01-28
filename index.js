bc9e16c5fbb8d2468b1ec1e97ea01cfba2b38eb5
jasonpham26 <vinhphuong0206@gmail.com>
2019-01-28 18:01:22 +1000

    Added mongo

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 require('./services/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+mongoose.connect(
+  keys.mongoURI,
+  () => {
+    console.log('Connected to MongoDB');
+  }
+);
 
 const app = express();
 
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`App started on port ${PORT}`);
+});
