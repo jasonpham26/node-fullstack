dfb197ec57374e08b2f4d0831acf26b5f6b2bbb3
Jason <vinhphuong0206@gmail.com>
2019-01-30 13:43:01 +1000

    Finished auth flow

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
-require('./services/passport');
+const cookieSession = require('cookie-session');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+
 mongoose.connect(
   keys.mongoURI,
   () => {
@@ -11,9 +15,21 @@ mongoose.connect(
 
 const app = express();
 
+// Make use of cookie
+app.use(
+  cookieSession({
+    // How long cookie gonna exist
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`App started on port ${PORT}`);
 });
